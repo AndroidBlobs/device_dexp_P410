8e9673c93f3e0c332af7f0f79fd9a750d6c18812
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-11 14:50:55 +0000

    s8631e_1g-user 8.1.0 OPM2.171019.012 19109 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/dexp/P410
+BOARD_VENDOR := dexp
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/dexp/P410/BoardConfigVendor.mk
